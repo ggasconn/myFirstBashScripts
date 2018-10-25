e71461ccb086591c9690da648cf3fb9403a4f25e
Guillermo GC <guillermo.gasconsmr@gmail.com>
2018-10-25 08:51:46 +0200

    New exercises

--- examExer.sh
+++ examExer.sh
@@ -6,20 +6,29 @@ if [ $# -ne 3 ]; then
   exit 1
 fi
 
-groupadd $3
-if ! [[ -f /root/logs/usersLog.txt ]]; then
-  touch /root/logs/usersLog.txt
+if grep -q $3 /etc/group; then
+  printf "\nGroup $3 already exists, skipping...\n"
+else
+   groupadd $3
 fi
+
 if ! [[ -d /root/logs/ ]]; then
   mkdir /root/logs/
 fi
 
+if ! [[ -f /root/logs/usersLog.txt ]]; then
+  touch /root/logs/usersLog.txt
+fi
+
 for ((i=0; i<=9; i++)); do
-  useradd $1-$i -p $2 -G $3
-  echo "- Created user: $1-$i at `date` - " >> /root/logs/usersLog.txt
+  useradd -m -k /etc/skel -G $3 $1-$i
+  echo "$1-$i:$2" | chpasswd
+  if [ $? == 0 ]; then
+    echo "- Created user: $1-$i at `date` -" >> /root/logs/usersLog.txt
+  fi
 done
 
-if [ -d $2 ]; then
+if [ -d $3 ]; then
   printf "\n\tError creating the specified directoy. Already exists\n"
 else
   mkdir /home/$3
